63852a832998ccdae9be60f877ebdc409296beb1
chanya.deshani <chanya.deshani@gmail.com>
2023-09-19 15:44:59 +0100

    get request

--- main.py
+++ main.py
@@ -1,27 +1,44 @@
-from flask import Flask
-from flask import request, escape
+from flask import Flask, render_template
+from flask import request
+from markupsafe import escape
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    celsius = str(escape(request.args.get("celsius", "")))
+    celsius = request.args.get("celsius", "")
+    if celsius:
+        fahrenheit = fahrenheit_from(celsius)
+    else:
+        fahrenheit = ""
     return (
         """<form action="" method="get">
-                <input type="text" name="celsius">
-                <input type="submit" value="Convert">
+                Celsius temperature: <input type="text" name="celsius">
+                <input type="submit" value="Convert to Fahrenheit">
             </form>"""
-        + celsius
+        + "Fahrenheit: "
+        + fahrenheit
     )
 
-@app.route("/<celsius>")
+
+'''
+    """ < form action="" method="get" >
+                < label for ="id" > Username: < / label >
+                < input type="text" name="id" id = "id" > < br >
+                <label for="password"> Password:</label>
+                <input type="password" name="password", id ="password"><br><br>
+                <input type="submit" value="Convert">
+            </form>"""                
+                
+                
+                
+                '''
+@app.route("/<int:celsius>")
 def fahrenheit_from(celsius):
     """Convert Celsius to Fahrenheit degrees."""
-    try:
-        fahrenheit = float(celsius) * 9 / 5 + 32
-        fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
-        return str(fahrenheit)
-    except ValueError:
-        return "invalid input"
+    fahrenheit = float(celsius) * 9 / 5 + 32
+    fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
+    return str(fahrenheit)
 
 
 if __name__ == "__main__":
